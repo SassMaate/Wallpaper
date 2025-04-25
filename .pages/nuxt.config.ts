e3915d78862c3732b78cf8b4a3411e310e0fddc7
Taiizor <41683699+Taiizor@users.noreply.github.com>
2025-04-25 03:02:31 +0300

    Add analytics and tracking scripts to Nuxt config
    Integrated Microsoft Clarity, Google Analytics, and Yandex Metrika scripts into the Nuxt configuration. These additions enable tracking and analytics for the application, including webvisor, clickmap, and other metrics.

--- .pages/nuxt.config.ts
+++ .pages/nuxt.config.ts
@@ -84,6 +84,71 @@ export default defineNuxtConfig({
       ],
       link: [
         { rel: 'icon', type: 'image/x-icon', href: '/Sucrose/favicon.ico' }
+      ],
+      script: [
+        // Microsoft Clarity
+        {
+          innerHTML: `
+            (function(c, l, a, r, i, t, y) {
+                c[a] = c[a] || function() {
+                    (c[a].q = c[a].q || []).push(arguments)
+                };
+                t = l.createElement(r);
+                t.async = 1;
+                t.src = "https://www.clarity.ms/tag/" + i;
+                y = l.getElementsByTagName(r)[0];
+                y.parentNode.insertBefore(t, y);
+            })(window, document, "clarity", "script", "pl85dw6k1x");
+          `,
+          type: 'text/javascript'
+        },
+        // Google Analytics
+        {
+          src: 'https://www.googletagmanager.com/gtag/js?id=G-5DZ26CZWDD',
+          async: true
+        },
+        {
+          innerHTML: `
+            window.dataLayer = window.dataLayer || [];
+            function gtag() {
+                dataLayer.push(arguments);
+            }
+            gtag('js', new Date());
+            gtag('config', 'G-5DZ26CZWDD');
+          `,
+          type: 'text/javascript'
+        },
+        // Yandex Metrika
+        {
+          innerHTML: `
+            (function(m, e, t, r, i, k, a) {
+                m[i] = m[i] || function() {
+                    (m[i].a = m[i].a || []).push(arguments)
+                };
+                m[i].l = 1 * new Date();
+                for (var j = 0; j < document.scripts.length; j++) {
+                    if (document.scripts[j].src === r) {
+                        return;
+                    }
+                }
+                k = e.createElement(t), a = e.getElementsByTagName(t)[0], k.async = 1, k.src = r, a.parentNode.insertBefore(k, a)
+            })
+            (window, document, "script", "https://mc.yandex.ru/metrika/tag.js", "ym");
+            ym(99351025, "init", {
+                webvisor: true,
+                clickmap: true,
+                trackLinks: true,
+                accurateTrackBounce: true
+            });
+          `,
+          type: 'text/javascript'
+        }
+      ],
+      noscript: [
+        {
+          innerHTML: `<div><img src="https://mc.yandex.ru/watch/99351025" style="position:absolute; left:-9999px;" alt="Yandex Metrika" /></div>`,
+          tagPosition: 'bodyClose'
+        }
       ]
     },
     baseURL: '/Sucrose/',
