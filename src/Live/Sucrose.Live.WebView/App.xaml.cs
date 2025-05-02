dcf499bcac91298fd3d509474fc20de629106bb4
Taiizor <41683699+Taiizor@users.noreply.github.com>
2025-05-02 20:36:33 +0300

    New Pre-Release

--- src/Live/Sucrose.Live.WebView/App.xaml.cs
+++ src/Live/Sucrose.Live.WebView/App.xaml.cs
@@ -163,7 +163,7 @@ namespace Sucrose.Live.WebView
                 }
                 else
                 {
-                    if (SHV.Compare(SHV.Clear(Version), SHV.Clear("131.0.2903.146")) != SEVT.Latest)
+                    if (SHV.Compare(SHV.Clear(Version), SHV.Clear("131.0.2903.112")) != SEVT.Latest)
                     {
                         return true;
                     }
