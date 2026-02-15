020420e49b0fe550f5a2b4fd32a5be9b59548c1e
Taiizor <41683699+Taiizor@users.noreply.github.com>
2026-02-15 17:33:57 +0300

    Add MIME types for .xhtml and .shtml
    Register .xhtml as application/xhtml+xml and .shtml as text/html in the WatsonWebServer content type mapping so those files are served with correct Content-Type headers.

--- src/Shared/Engine/Sucrose.Shared.Engine.CefSharp/Extension/WatsonWebServer.cs
+++ src/Shared/Engine/Sucrose.Shared.Engine.CefSharp/Extension/WatsonWebServer.cs
@@ -123,6 +123,7 @@ namespace Sucrose.Shared.Engine.CefSharp.Extension
             return Path.GetExtension(filename).ToLowerInvariant() switch
             {
                 ".rar" => "application/x-rar-compressed",
+                ".xhtml" => "application/xhtml+xml",
                 ".js" => "application/javascript",
                 ".hdr" => "image/vnd.radiance",
                 ".tar" => "application/x-tar",
@@ -138,6 +139,7 @@ namespace Sucrose.Shared.Engine.CefSharp.Extension
                 ".webp" => "image/webp",
                 ".webm" => "video/webm",
                 ".tiff" => "image/tiff",
+                ".shtml" => "text/html",
                 ".jpeg" => "image/jpeg",
                 ".woff" => "font/woff",
                 ".txt" => "text/plain",
